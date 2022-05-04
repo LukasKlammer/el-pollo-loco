20c1fbaf572cb630ed4472672da86655ee8dcb85
LukasKlammer <klammer.lukas@hotmail.com>
2022-05-04 17:06:51 +0200

    move objects, image cache

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -8,6 +8,16 @@ class Cloud extends MovableObject {
         super().loadImage('../img/5.Fondo/Capas/4.nubes/1.png');
 
         this.x = Math.random() * 500; // Zahl zwischen 0 und 500
+
+        this.animate();
     }
+
+    animate() {
+        setInterval(() => {
+            this.x -= 0.1; // was soll ausgeführt werden
+        }, 1000 / 60); // 2. Parameter Millisekunden, also alle wieviel Zeit wiederholt wird
+
+    }
+
 
 }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,11 +4,27 @@ class MovableObject {
     img;
     height = 150;
     width = 120;
+    imageCache = {};
 
     // loadImage('img/test.png');
     loadImage(path) {
         this.img = new Image(); // this.img = document.getElementById('image') <img id="image" src...>
         this.img.src = path;
+    }
+
+
+    /**
+     * loads all images from the given array in the variable imageCache, which is a JSON
+     * 
+     * @param {Array} arr ['img/image1.png', 'img/image2.png', ...]
+     */
+    loadImages(arr) {
+        arr.forEach(path => {
+            let img = new Image(); // Variable mit neuem Bild wird angelegt
+            img.src = path; // Bild (Pfad) wird ins img Objekt reingeladen
+            this.imageCache[path] = path; // this ist wichtig, weil wir auf Variable von Objekt zugreifen, nicht auf einer von innerhalb der Funktion
+            // im imageCache werden Bilder reingeladen: Schlüssel ist der Datei-Pfad
+        });
     }
 
 
